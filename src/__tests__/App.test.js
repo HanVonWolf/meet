9ca7b788bf81fae5c7d6fb2a1055e0ae7ce1828e
HanVonWolf <hannahwolfenden@outlook.com>
2025-04-11 10:46:49 +0200

    commit task 4.4

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
+import { render, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { getEvents } from "../api";
+import App from "../App";
 
+describe("<App /> component", () => {
+	let AppDOM;
+	beforeEach(() => {
+		AppDOM = render(<App />).container.firstChild;
+	});
 
-describe('<App /> component', () => {
-  let AppDOM;
-  beforeEach(() => {
-    AppDOM = render(<App />).container.firstChild;
-  })
+	test("renders list of events", () => {
+		expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+	});
 
+	test("render CitySearch", () => {
+		expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
+	});
 
-  test('renders list of events', () => {
-    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
-  });
-
-
-  test('render CitySearch', () => {
-    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-  });
+	test("render NumberOfEvents", () => {
+		expect(AppDOM.querySelector("#number-of-events")).toBeInTheDocument();
+	});
 });
--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
 
-
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
+
+
   const handleInputChanged = (event) => {
     const value = event.target.value;
     const filteredLocations = allLocations ? allLocations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     }) : [];
-    const handleItemClicked = (event) => {
-        const value = event.target.textContent;
-        setQuery(value);
-        setShowSuggestions(false); // to hide the list
-      };
-
-
     setQuery(value);
     setSuggestions(filteredLocations);
+  };
+
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false);
+    setCurrentCity(value);
   };
 
   return (
@@ -43,8 +48,7 @@ const CitySearch = ({ allLocations }) => {
         : null
       }
     </div>
- )
+  )
 }
-
 
 export default CitySearch;
